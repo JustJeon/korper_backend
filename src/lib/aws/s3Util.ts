12c2dfc32e69f6e19135863c5cb954d23431ecef
grr1203 <gyrms9412@naver.com>
2024-03-04 20:02:37 +0900

    project init

---
+++ src/lib/aws/s3Util.ts
@@ -0,0 +1,48 @@
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client, GetObjectCommand, HeadObjectCommand } from '@aws-sdk/client-s3';
+
+const client = new S3Client({ region: 'ap-northeast-2' });
+const Bucket = 'zzip';
+
+export const getPresignedPostUrl = async (Key: string) => {
+  const { url, fields } = await createPresignedPost(client, {
+    Bucket,
+    Key,
+    Conditions: [],
+    Fields: {},
+    Expires: 600, // seconds
+  });
+  console.log('[getPresignedPostUrl]', { url, fields });
+
+  return { url, fields };
+};
+
+export const getPresignedUrl = async (Key: string) => {
+  const command = new GetObjectCommand({ Bucket, Key });
+  const url = await getSignedUrl(client, command, { expiresIn: 600 });
+  console.log('[getPresignedUrl]', url);
+
+  return url;
+};
+
+// object head(metadata 등) 정보 조회 or object 존재 여부 확인
+export const getHeadObject = async (Key: string) => {
+  let res;
+
+  try {
+    const command = new HeadObjectCommand({ Bucket, Key });
+    res = await client.send(command);
+    console.log('[getHeadObject]', res);
+  } catch (error) {
+    if (error.name === 'NotFound') {
+      console.log('Object does not exist.');
+      return { error: 'NotFound' };
+    } else {
+      console.error('Error:', error);
+      return { error };
+    }
+  }
+
+  return res;
+};
